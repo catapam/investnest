469a01fb9e14e694393900c889ad6f158fbe23fd
catapam <matheuscatapam@gmail.com>
2024-08-11 19:15:59 +0000

    Ready for deploy on Heroku

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
