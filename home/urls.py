026021b0bb4740cd32b58489556563d141cc55ed
catapam <matheuscatapam@gmail.com>
2024-08-16 13:51:34 +0000

    Create wireframes and content for home app

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Index
+from .views import Index, Wireframes
 
 
 urlpatterns=[
-    path('', Index.as_view(), name='home')
+    path('', Index.as_view(), name='home'),
+    path('wireframes/', Wireframes.as_view(), name='wireframes')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name= 'home/index.html'
+
+class Wireframes(TemplateView):
+    template_name= 'home/wireframes.html'
